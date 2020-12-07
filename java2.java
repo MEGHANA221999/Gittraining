a587954de0327bfd738e7432c7aca23d2ca94fcd
meghana <meghana.mgowda1999@gmail.com>
2020-12-07 11:58:15 +0530

    javaaaprgm

---
+++ java2.java
@@ -0,0 +1,22 @@
+package com.sonata;
+/*
+ * PRINT LARGEST NUMBER
+ */
+public class ASSIGNMENT_1 {
+
+	public static void main(String[] args) {
+		
+		int  a= 12, b = 25, c = 89;
+
+		        if( a >= b && a >= c)
+		            System.out.println(a + " is the largest number.");
+
+		        else if (b >= a && b >= c)
+		            System.out.println(b + " is the largest number.");
+
+		        else
+		            System.out.println(c + " is the largest number.");
+	 
+		}
+
+}
